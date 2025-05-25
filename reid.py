ee5278c8c1211b3b7d5e3490319de2d33e310a9d
eigenvectorBazuz <felix.goldberg@gmail.com>
2025-05-25 12:40:23 +0300

    Update reid.py

--- reid.py
+++ reid.py
@@ -1,1 +1,14 @@
+import torch
 
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+from torchreid.utils import FeatureExtractor
+
+extractor = FeatureExtractor(
+    model_name = 'osnet_ain_x1_0',  # strong vehicle‐ReID backbone
+    device = device            # or 'cpu'
+)
+
+# the idea is to use reid features to see if two tracks refer to the same objects - either within the same video or across different videos.
+def CompareTracks(t1, t2):
+  
