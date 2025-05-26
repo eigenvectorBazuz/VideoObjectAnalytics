ffc021c1223c69b8e6b11235921427aa95144bb7
eigenvectorBazuz <felix.goldberg@gmail.com>
2025-05-26 13:27:40 +0300

    Update script_video_process.py

--- script_video_process.py
+++ script_video_process.py
@@ -1,1 +1,7 @@
+from multiple_video_analysis import analyze_and_annotate_videos
 
+videos = ['/content/drive/MyDrive/BR/seg0.mp4', '/content/drive/MyDrive/BR/seg1.mp4', '/content/drive/MyDrive/BR/seg2.mp4']
+
+resdir = '/content/drive/MyDrive/BR/ids_res_2605'
+
+analyze_and_annotate_videos(videos, resdir)
