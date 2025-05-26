f5209dd740b47166f46117dd1137409d9de3c601
eigenvectorBazuz <felix.goldberg@gmail.com>
2025-05-26 10:15:59 +0300

    Update multiple_movie_analysis.py

--- multiple_movie_analysis.py
+++ multiple_movie_analysis.py
@@ -2,4 +2,11 @@ from single_movie_analysis import discover_objects_in_video
 from reid import Encode, CompareTrackList
 from utils import annotate_video_with_tracks
 
+def analyze_and_annotate_videos(videos_list):
+  results = []
+  for video in videos_list:
+    tracks = discover_objects_in_video(video, 'yolo12x.pt', tie_point_params={'video_chunk_size':50, 'overlap':5, 'grid_size':20}, 
+                               include_final_split=False, return_data=False)
+    results.append(tracks)
+
 
