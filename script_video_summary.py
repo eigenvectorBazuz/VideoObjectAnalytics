4216dd71b839169e6d7b312c8d09bead83083f4f
eigenvectorBazuz <felix.goldberg@gmail.com>
2025-05-26 09:00:36 +0300

    Update script_video_summary.py

--- script_video_summary.py
+++ script_video_summary.py
@@ -1,1 +1,5 @@
+from video_summary import VideoChatBot
 
+clip_file = '/content/drive/MyDrive/BR/seg0.mp4'
+
+vcb = VideoChatBot(size = "0.5B", quant_4bit = True, target_hw = (360, 480), frames_per_chunk = 30)
