f24d513b156bdfb9351b9a13feb60b3b42f0b822
eigenvectorBazuz <felix.goldberg@gmail.com>
2025-05-26 09:00:55 +0300

    Update script_video_summary.py

--- script_video_summary.py
+++ script_video_summary.py
@@ -3,3 +3,9 @@ from video_summary import VideoChatBot
 clip_file = '/content/drive/MyDrive/BR/seg0.mp4'
 
 vcb = VideoChatBot(size = "0.5B", quant_4bit = True, target_hw = (360, 480), frames_per_chunk = 30)
+
+user_prompt = """ Identify key events in the segment (e.g., object appearances, interactions, unusual movements, bridges ... ), in particular vehicle maneuvres.
+"""
+report = vcb(clip_file, user_prompt)
+
+print(report)
